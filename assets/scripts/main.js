a610286d99b103c1b5f53fb0700049f511ec9931
kay8 <kay.yamagishi.work@gmail.com>
2017-09-17 00:20:49 -0700

    wip

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,68 +1,69 @@
 const map = document.getElementById("map");
-
+const originalView = "0 0 1282.92 1243.61";
 // interaction
-function zoomIn(country) {
+function zoomIn(province) {
 // zooming in part
-var currentCountry = document.getElementById(country),
-    s = currentCountry.getBBox(),
-    newView = "" + s.x + " " + s.y + " " + (s.width + 200) + " " + s.height,
-    group1 = [".text-" + country, ".x-out"],
-    tl = new TimelineMax();
+  const currentProvince = document.getElementById(province);
+  const s = currentProvince.getBBox();
+  //const newView = "" + s.x + " " + s.y + " " + s.width + " " + s.height;
+  const newView = `${s.x} ${s.y} ${s.width} ${s.height}`;
+  const group1 = [".text-" + province, ".x-out"];
+  tl = new TimelineMax();
 
-    tl.add("zIn");
-    tl.fromTo(map, 1.5, {
-      attr: { viewBox: "0 0 1282.95 1243.58"}
-    }, {
-      attr: { viewBox: newView }
-    }, "zIn");
-    tl.to(".text-" + country, 0.1, {
-      display: "block"
-    }, "zIn");
-    tl.fromTo(group2, 0.25, {
-      opacity: 1
-    }, {
-      opacity: 0,
-      ease: Circ.easeIn
-    }, "zIn");
-    tl.fromTo(currentCountry, 0.35, {
-      opacity: 0
-    }, {
-      opacity: 1,
-      ease: Circ.easeOut
-    }, "zIn+=0.5");
-    tl.fromTo(group1, 0.5, {
-      opacity: 0
-    }, {
-      opacity: 0.65,
-      ease: Sine.easeOut
-    }, "zIn+=1");
+  tl.add("zIn");
+  tl.fromTo(map, 1.5, {
+    attr: { viewBox: originalView}
+  }, {
+    attr: { viewBox: newView }
+  }, "zIn");
+  tl.to(".text-" + country, 0.1, {
+    display: "block"
+  }, "zIn");
+  tl.fromTo(group2, 0.25, {
+    opacity: 1
+  }, {
+    opacity: 0,
+    ease: Circ.easeIn
+  }, "zIn");
+  tl.fromTo(currentCountry, 0.35, {
+    opacity: 0
+  }, {
+    opacity: 1,
+    ease: Circ.easeOut
+  }, "zIn+=0.5");
+  tl.fromTo(group1, 0.5, {
+    opacity: 0
+  }, {
+    opacity: 0.65,
+    ease: Sine.easeOut
+  }, "zIn+=1");
 }
 
 function zoomOut(geo) {
-//zooming out part
-var currentArea = document.getElementById(geo),
-    group3 = [".text-" + geo, ".x-out"],
-    tl = new TimelineMax();
+  //zooming out part
+  const currentArea = document.getElementById(geo);
+  group3 = [".text-" + geo, ".x-out"]
+  const tl = new TimelineMax();
 
-    tl.add("zOut");
-    tl.to(group3, 0.5, {
-      opacity: 0,
-      ease: Sine.easeIn
-    }, "zOut");
-    tl.to(map, 1, {
-      attr: { viewBox: "0 0 1282.95 1243.58"}
-    }, "zOut");
-    tl.to(group2, 0.25, {
-      opacity: 1,
-      ease: Sine.easeOut
-    }, "zOut+=1");
-    tl.to(".text-" + geo, 0.1, {
-      display: "none"
-    }, "zOut+=2");
-    tl.to(currentArea, 1, {
-      opacity: 0,
-      ease: Sine.easeIn
-    }, "zOut+=0.4");
+  tl.add("zOut");
+  tl.to(group3, 0.5, {
+    opacity: 0,
+    ease: Sine.easeIn
+  }, "zOut");
+  tl.to(map, 1, {
+    attr: { viewBox: originalView}
+  }, "zOut");
+  tl.to(group2, 0.25, {
+    opacity: 1,
+    ease: Sine.easeOut
+  }, "zOut+=1");
+  tl.to(".text-" + geo, 0.1, {
+    display: "none"
+  }, "zOut+=2");
+  tl.to(currentArea, 1, {
+    opacity: 0,
+    ease: Sine.easeIn
+  }, "zOut+=0.4");
 
 }
 
