4e7571a90dd611473919cf20c7322b8b7264ab03
kay8 <kay.yamagishi.work@gmail.com>
2017-09-17 11:27:06 -0700

    zoom in and out BC

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,81 +1,53 @@
 const map = document.getElementById("map");
 const originalView = "0 0 1282.92 1243.61";
-// interaction
+
 function zoomIn(province) {
-// zooming in part
-  const currentProvince = document.getElementById(province);
-  const s = currentProvince.getBBox();
-  //const newView = "" + s.x + " " + s.y + " " + s.width + " " + s.height;
+  const s = province.getBBox();
   const newView = `${s.x} ${s.y} ${s.width} ${s.height}`;
-  const group1 = [".text-" + province, ".x-out"];
   tl = new TimelineMax();
 
   tl.add("zIn");
-  tl.fromTo(map, 1.5, {
+  tl.fromTo(map, 1, {
     attr: { viewBox: originalView}
   }, {
     attr: { viewBox: newView }
   }, "zIn");
-  tl.to(".text-" + country, 0.1, {
-    display: "block"
-  }, "zIn");
-  tl.fromTo(group2, 0.25, {
-    opacity: 1
-  }, {
-    opacity: 0,
-    ease: Circ.easeIn
-  }, "zIn");
-  tl.fromTo(currentCountry, 0.35, {
-    opacity: 0
-  }, {
-    opacity: 1,
-    ease: Circ.easeOut
-  }, "zIn+=0.5");
-  tl.fromTo(group1, 0.5, {
-    opacity: 0
-  }, {
-    opacity: 0.65,
-    ease: Sine.easeOut
-  }, "zIn+=1");
 }
 
-function zoomOut(geo) {
-  //zooming out part
-  const currentArea = document.getElementById(geo);
-  group3 = [".text-" + geo, ".x-out"]
+function zoomOut() {
   const tl = new TimelineMax();
 
   tl.add("zOut");
-  tl.to(group3, 0.5, {
-    opacity: 0,
-    ease: Sine.easeIn
-  }, "zOut");
   tl.to(map, 1, {
     attr: { viewBox: originalView}
   }, "zOut");
-  tl.to(group2, 0.25, {
-    opacity: 1,
-    ease: Sine.easeOut
-  }, "zOut+=1");
-  tl.to(".text-" + geo, 0.1, {
-    display: "none"
-  }, "zOut+=2");
-  tl.to(currentArea, 1, {
-    opacity: 0,
-    ease: Sine.easeIn
-  }, "zOut+=0.4");
-
 }
 
-var el = document.getElementById("CA-BC");
-el.addEventListener("click", callback, false);
+const close = document.getElementById("close");
+const hotspots = document.querySelectorAll(".hotspot");
+const titleProvince = document.querySelector(".title__province");
 
-var close = document.getElementById("close");
-close.addEventListener("click", callback2, false);
+Array.from(hotspots).forEach(hotspot => {
+  hotspot.addEventListener("click", function() {
+    const hotspotData = this.dataset;
+    const currentProvince = document.getElementById(hotspotData.parent);
+    zoomIn(currentProvince);
 
-function callback() {
-  zoomIn("CA-BC");
-}
-function callback2() {
-  zoomOut("aaa");
-}
+    //show and hide
+    currentProvince.classList.add("is-active-area");
+    titleProvince.innerHTML = `- ${hotspotData.info}`;
+    const img = document.getElementById(`${hotspotData.parent}-img`);
+    img.classList.add("is-active");
+    close.classList.remove("is-hidden");
+  }, false);
+});
+
+close.addEventListener("click", function() {
+  zoomOut();
+
+  //show and hide
+  close.classList.add("is-hidden");
+  titleProvince.innerHTML = "";
+  document.querySelector("img.is-active").classList.remove("is-active");
+  document.querySelector(".is-active-area").classList.remove("is-active-area");
+}, false);
